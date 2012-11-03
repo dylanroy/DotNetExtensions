bb2395bd73244a9935fe34f4f020039ee466d364
dkroy <dylan.k.roy@gmail.com>
2012-11-03 12:04:42 -0500

    Added minimal comments, and future methods.

--- DotNetExtensions/StringExtensions.cs
+++ DotNetExtensions/StringExtensions.cs
@@ -4,14 +4,46 @@ using System.Text;
 
 namespace DotNetExtensions
 {
+    /// <summary>
+    /// This class provides useful extensions for the String Object.
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// More similar to Java version of substring, except removes the step of calling index of.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="startStringMatch"></param>
+        /// <param name="endStringMatch"></param>
+        /// <returns></returns>
         public static string SubString(this string str, string startStringMatch, string endStringMatch){
             return str.Substring(str.IndexOf(startStringMatch), str.IndexOf(endStringMatch) - str.IndexOf(startStringMatch));
         }
+        /// <summary>
+        /// More similar to Java version of substring.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex"></param>
+        /// <returns></returns>
         public static string SubString(this string str, int startIndex, int endIndex)
         {
             return str.Substring(startIndex, endIndex - startIndex);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        //Find
+        public static string Search(this String str){
+            //Boyer-Moore Algorithm
+            return null;
+        }
+        //Contains with fast search
     }
 }
